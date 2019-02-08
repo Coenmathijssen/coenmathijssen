8ef244ef8a396de091688ad8388de8b681a37432
Coen <coen.skateboard@gmail.com>
2019-02-08 16:46:54 +0100

    Ik heb flickity slider toegevoegd en wat aanpassingen op home gedaan

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,20 @@
-import '../styles/index.scss';
+import '../styles/index.scss'
 
-console.log('webpack starterkit');
+// console.log('webpack starterkit');
+import Flickity from 'flickity'
+
+let elem = document.querySelector('.main-carousel')
+var flkty = new Flickity ( elem, {
+  // options
+  cellAlign: 'left',
+  imagesLoaded: true
+  // contain: true
+})
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity ( '.main-carousel', {
+  // options
+})
+
+
